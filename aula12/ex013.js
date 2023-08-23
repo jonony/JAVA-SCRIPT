2e44f4a2c8aaa36f139361f86f9b943c0b0975d5
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-08-23 20:03:07 -0300

    Update ex013.js

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,29 +1,51 @@
-var agora = new Date();
-var diaSem = agora.getDay();
+var agora = new Date()
+var now = agora.getHours()
 
-diaSem = 9
-switch (diaSem) {
-    case 0:
-        console.log("Hoje é Domingo");
-        break;
-    case 1:
-        console.log("Hoje é Segunda-feira");
-        break;
-    case 2:
-        console.log("Hoje é terça-feira");
-        break;
-    case 3:
-        console.log("Hoje é quarta-feira");
-        break;
-    case 4:
-        console.log("Hoje é quinta-feira");
-        break;
-    case 5:
-        console.log("Hoje é sexta-feira");
-        break;
-    case 6:
-        console.log("Hoje é Sábado");
-        break;
-    default:
-        console.log('Dia invalido')
+var hours = new Date()
+var hour  = hours.getHours()
+
+var mins = new Date()
+var min = mins.getMinutes()
+
+var semana = new Date()
+var diaSem = semana.getDay()
+
+if(now < 12){
+    console.log('Bom dia!')
 }
+else if(now <= 18){
+    console.log('Boa tarde!')
+}
+else{
+    console.log('Boa noite!')
+}
+
+
+switch(diaSem){
+    case 0:
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Domingo!`)
+        break
+    case 1:
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Segunda-feira!`)
+        break
+    case 2: 
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Terça-feira! `)
+        break
+    case 3: 
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Quarta-feira! `)
+        break
+    case 4:
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Quinta-feira! `)
+        break
+    case 5:
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Sexta-feira! `)
+        break
+    case 6:
+        console.log(`Agora são exatamente ${hour} horas e ${min} minutos! Hoje é Sábado!`)
+        break
+    default:
+        console.log('[ERRO] Dia não encontrado')
+    }
+    
+
+
