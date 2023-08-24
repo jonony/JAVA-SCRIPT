537701b1a6f4c2bffceb6060c50d90e4f3fc5d5f
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-08-24 07:40:01 -0300

    comit

--- Exercicios-JS/reset/script.js
+++ Exercicios-JS/reset/script.js
@@ -9,27 +9,27 @@ function atualizar(){
     var min = new Date()
     var minuto = min.getMinutes()
 
-    var day = new Date()
-    var hoje = 0//day.getDay()
+    msg.innerHTML = `Aqui vai aparecer a mensagem`
 
-    msg.innerHTML = null
-
-   if(hora < 12 ){
-    msg.innerHTML = `Bom dia! Agora são ${hora} horas e ${minuto} minutos. Hoje é ${hoje}!`
+    if(hora < 12){
+    msg.innerHTML = `Bom dia! Agora são ${hora} horas e ${minuto} minutos!`
     img.src = 'fotos/manha.png'
    }
 
    else if(hora >= 12 && hora <= 18){
-    msg.innerHTML += `Boa tarde! Agora são ${hora} horas e ${minuto}minutos. Hoje é ${hoje}!`
+    msg.innerHTML += `Boa tarde! Agora são ${hora} horas e ${minuto}minutos!`
     img.src = 'fotos/tarde.png'
    }
 
    else{
-    msg.innerHTML += `Boa noite! Agora são ${hora} horas e ${minuto} minutos. Hoje é ${hoje}!`
+    msg.innerHTML += `Boa noite! Agora são ${hora} horas e ${minuto} minutos!`
     img.src = 'fotos/noite.png'
    }
+
    
+
    
+  
 }
 
    
