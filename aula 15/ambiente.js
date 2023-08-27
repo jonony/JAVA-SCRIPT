2a0d6704ca23fbc7b31e7dc634034e17d5770544
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-08-27 15:31:42 -0300

    move

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,5 +1,25 @@
+
 var c  = 1
 
-for(c=1;c<10;c++){
+for(c=1;c<6;c++){
     console.log(`Regra ${c}`)
 }
+
+/*
+    var c = 1
+
+    do{
+        console.log(`Regra ${c}`)
+        c++
+    }while(c<6)
+*/
+
+ 
+/*
+    var c = 1
+
+    while(c<6){
+        console.log(`Regra ${c}`)
+        c++
+    }
+*/    
