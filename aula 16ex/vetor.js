8cfb4d519b03f58ab668e1581617db1ed9c68e7c
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-08-31 21:28:54 -0300

    criei novos exercicios

--- aula 16ex/vetor.js
+++ aula 16ex/vetor.js
@@ -1,18 +1,14 @@
 let valores = [8,1,7,4,2,9]
 
-/*for(let pos = 0; pos < valores.length; pos++){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
-*/
-
 for(let pos in valores){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    console.log(`A posição ${pos} possui o valor ${valores[pos]}`)
 }
+
 let pos = valores.indexOf(2)
 
 if(pos == -1){
-    console.log('O valor não foi encontrado!')
+    console.log('Infelizmente esse número não existe!')
 }
 else{
-    console.log(`O valor se encontra na posição ${pos} com o valor ${valores[pos]}`)
+    console.log(`A posição ${pos} possui o valor ${valores[pos]}`)
 }
