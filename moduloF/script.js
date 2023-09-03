9c2bd035ba3a9b4dda71c973fa5a772b9f391513
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-09-03 09:47:34 -0300

    update

--- moduloF/script.js
+++ moduloF/script.js
@@ -1,7 +1,27 @@
 let num = window.document.getElementById('txtnum')
+let lista = window.document.getElementById('flista')
+let res = window.document.getElementById('res')
+let valores = []
+
+function isNumero (n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n,l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
 
 function adicionar(){
-    if(isNumber(num.value) && !inLista(num.value, valores)){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
 
 }else{
     alert('Valor inválido, ou já encontrado na lista')
