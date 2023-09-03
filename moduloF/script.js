e1144df2b5e79a45b0a7521a83a62d731f5fba06
Johny Carvalho <134290213+jonony@users.noreply.github.com>
2023-09-03 09:16:51 -0300

    refazendo

--- moduloF/script.js
+++ moduloF/script.js
@@ -1,32 +1,9 @@
-let num = document.getElementById('txtnum')
-let lista = document.getElementById('flista')
-let res = document.getElementById('res')
-let valores = []
-
-function isNumero(n){
-  if(Number(n) >= 1 && Number(n) <= 100){
-    return true
-  }else{
-    return false
-  }      
-}
-
-function inLista(n,l){
-   if(l.indexOf(Number(n)) != -1){
-    return true
-   }else{
-    return false
-   }
-}
+let num = window.document.getElementById('txtnum')
 
 function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
-    }else{
-        alert('Valor inválido, ou já encontrado na lista')
-    }num.value = ''
-    num.focus()
+    if(isNumber(num.value) && !inLista(num.value, valores)){
+
+}else{
+    alert('Valor inválido, ou já encontrado na lista')
+}
 }
